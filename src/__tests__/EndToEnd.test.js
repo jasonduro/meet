a9d1d36af22fe4d149f2c789096e7f1ed0848ced
Jason <product@jasonlamanque.com>
2023-04-27 16:59:25 +0200

    update refactor e2e test

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,34 +1,28 @@
 import puppeteer from 'puppeteer';
 
-beforeAll(async () => { 
-jest.setTimeout(30000); // Set the timeout to 30 seconds
-});
-
 describe('show/hide an event details', () => {
-  
-  test('An event element is collapsed by default', async () => {
-    const browser = await puppeteer.launch();
-
-    const page = await browser.newPage();
+  let browser;
+  let page;
+  beforeAll(async () => {
+    jest.setTimeout(30000);
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
     await page.goto('http://localhost:3000/');
-
     await page.waitForSelector('.event');
+  });
 
+  afterAll(() => {
+    browser.close();
+  });
+
+  test('An event element is collapsed by default', async () => {
     const eventDetails = await page.$('.event .event__Details');
     expect(eventDetails).toBeNull();
-    browser.close();
-  }, 30000); // Set the timeout specifically for this test to 30 seconds)
+  });
 
   test('User can expand an event to see its details', async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('http://localhost:3000/');
-
-    await page.waitForSelector('.event');
-    await page.click('.event .details-btn');
-
+    await page.click('.event .detailsButton');
     const eventDetails = await page.$('.event .event__Details');
     expect(eventDetails).toBeDefined();
-    browser.close();
   });
 });
