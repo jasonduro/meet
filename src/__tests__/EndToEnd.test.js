1678667354c114170fb29b2b382b20140299b8e9
Jason <product@jasonlamanque.com>
2023-04-27 16:50:44 +0200

    added expand event test

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -5,6 +5,7 @@ jest.setTimeout(30000); // Set the timeout to 30 seconds
 });
 
 describe('show/hide an event details', () => {
+  
   test('An event element is collapsed by default', async () => {
     const browser = await puppeteer.launch();
 
@@ -17,4 +18,17 @@ describe('show/hide an event details', () => {
     expect(eventDetails).toBeNull();
     browser.close();
   }, 30000); // Set the timeout specifically for this test to 30 seconds)
+
+  test('User can expand an event to see its details', async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
+
+    await page.waitForSelector('.event');
+    await page.click('.event .details-btn');
+
+    const eventDetails = await page.$('.event .event__Details');
+    expect(eventDetails).toBeDefined();
+    browser.close();
+  });
 });
