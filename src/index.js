fe6ba87e46dff639131f66fc70247b4f7cedf317
Jason <product@jasonlamanque.com>
2023-04-28 13:50:56 +0200

    atatus integration

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('bf6c486e359e4e9e92f237de1493d142').install();
 
 const root = document.getElementById('root');
 ReactDOM.render(
@@ -22,3 +24,4 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
