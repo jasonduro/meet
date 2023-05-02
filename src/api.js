d1e35b38a662af55e5268c9ff3a4baac7494ff3b
Jason <product@jasonlamanque.com>
2023-05-02 14:54:12 +0200

    last event save for localstorage offline

--- src/api.js
+++ src/api.js
@@ -78,6 +78,12 @@ export const getEvents = async (numberOfResults) => {
     return mockData;
   }
 
+  if (!navigator.onLine) {
+    const data = localStorage.getItem("lastEvents");
+    NProgress.done();
+    return data?JSON.parse(events).events:[];;
+  }
+  
   const token = await getAccessToken();
 
   if (token) {
