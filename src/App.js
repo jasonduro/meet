42f906cdf64a7971096e02f9a10092a8c5b76d51
Jason <product@jasonlamanque.com>
2023-05-04 21:16:36 +0200

    update repsonsive chart

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import { getEvents, extractLocations, checkToken, getAccessToken } from './api';
 import WelcomeScreen from './WelcomeScreen';
-import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 
 class App extends Component {
@@ -87,20 +87,16 @@ class App extends Component {
         <br></br>
         <NumberOfEvents numberOfResults={this.state.numberOfResults} updateNumberOfResults={this.updateNumberOfResults} />
         <h4>Events in each city</h4>
-
-        <ScatterChart
-        width={800}
-        height={400}
-        margin={{
-          top: 20, right: 20, bottom: 20, left: 20,
-        }}
-        >
+        
+        <ResponsiveContainer height={400}>
+        <ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
         <CartesianGrid />
         <XAxis type="category" dataKey="city" name="city" />
         <YAxis type="number" dataKey="number" name="# of events" allowDecimals={false} />
         <Tooltip cursor={{ strokeDasharray: '3 3' }} />
         <Scatter data={this.getData()} fill="#8884d8" />
         </ScatterChart>
+        </ResponsiveContainer >
 
         <EventList 
           events={this.state.events}
